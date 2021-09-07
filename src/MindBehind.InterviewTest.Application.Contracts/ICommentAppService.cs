b65acd1d5f9f7f40508ce28082c1c4d96502fdea
Hasan Baidoun <eng.hasan.baidoun@outlook.com>
2021-09-07 10:19:13 +0300

    add comment service

---
+++ src/MindBehind.InterviewTest.Application.Contracts/ICommentAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MindBehind.InterviewTest.Dto;
+using MindBehind.InterviewTest.Localization;
+using Volo.Abp.Application.Services;
+
+namespace MindBehind.InterviewTest.Application.Contracts
+{
+    public interface ICommentAppService : IApplicationService
+    {
+        Task<List<CommentDto>> GetListAsync();
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ src/MindBehind.InterviewTest.Application/CommentAppService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MindBehind.InterviewTest.Application.Contracts;
+using MindBehind.InterviewTest.Dto;
+using MindBehind.InterviewTest.Localization;
+using Volo.Abp;
+using RestSharp;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace MindBehind.InterviewTest
+{
+    /* Inherit your application services from this class.
+     */
+    public class CommentAppService : InterviewTestAppService, ICommentAppService
+    {
+        public CommentAppService() : base()
+        {
+
+        }
+
+        public async Task<List<CommentDto>> GetListAsync()
+        {
+            try
+            {
+                var client = new RestClient("https://my-json-server.typicode.com");
+                var request = new RestRequest("typicode/demo/comments", DataFormat.Json);
+                var data = await client.GetAsync<List<CommentExtDto>>(request);
+
+                var result = ObjectMapper.Map<List<CommentExtDto>, List<CommentDto>>(data);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException("CommentAppService.GetListAsync", "Error fetching comments", innerException : ex, logLevel : LogLevel.Error);
+            }
+            
+
+            //return await Task.FromResult(new List<CommentDto> { 
+            //    new CommentDto { Id = 1, PostId = 1, Body = "hasan" }
+            //});
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new BusinessException("CommentAppService.DeleteAsync", "DELETE method is not yet implemented");
+        }
+    }
+}

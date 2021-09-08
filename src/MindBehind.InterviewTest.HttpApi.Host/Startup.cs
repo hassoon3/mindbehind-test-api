5350671e5d264c91d8972fd854081c8fcd1bf069
Hasan Baidoun <eng.hasan.baidoun@outlook.com>
2021-09-09 00:10:50 +0300

    add middleware

--- src/MindBehind.InterviewTest.HttpApi.Host/Startup.cs
+++ src/MindBehind.InterviewTest.HttpApi.Host/Startup.cs
@@ -2,11 +2,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace MindBehind.InterviewTest
 {
     public class Startup
     {
+        private const string HeaderMbCount = "MB_COUNT";
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication<InterviewTestHttpApiHostModule>();
@@ -14,6 +17,22 @@ namespace MindBehind.InterviewTest
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            app.Use(async (context, next) =>
+            {
+                StringValues mbCount;
+                if (context.Request.Headers.TryGetValue(HeaderMbCount, out mbCount))
+                {
+                    int mbCountInt;
+                    if (int.TryParse(mbCount.ToString(), out mbCountInt))
+                    {
+                        context.Response.Headers[HeaderMbCount] = (mbCountInt + 1).ToString();
+                    }
+                }
+
+                await next();
+            });
+
             app.InitializeApplication();
         }
     }
